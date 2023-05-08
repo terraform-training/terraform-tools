772d5ab80ac3bd5d6dd979ee07f40575a9b5647a
Maciej Rostański <mrostanski@gmail.com>
2023-05-08 22:19:15 +0200

    checkov tests

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,19 @@ locals {
   }
 }
 
+## Keypair for bastion
 resource "aws_key_pair" "bastion_key" {
   key_name   = "bastion-key"
   public_key = var.bastion_key
 
   tags = local.tags
 }
+
+# ## Elastic IP for bastion
+# resource "aws_eip" "bastion_reserved_ip" {
+#   vpc = true
+
+#   tags = local.tags
+# }
+
+# ## suppress with comment: #checkov:skip=CKV2_AWS_19:We want to pay for this and have it reserved
